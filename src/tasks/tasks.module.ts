31845e2a85bc6a45ee06930fd2064bba91c6c3d8
Winsmer Oneal Marte Adames <wmarte@apap.com.do>
2022-03-19 14:28:11 -0400

    feat: repository pattern

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksController } from './controller/tasks.controller';
+import { TaskRepository } from './repository/task.repository';
+import { TasksService } from './service/tasks.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  controllers: [TasksController],
+  providers: [TasksService],
+})
 export class TasksModule {}
